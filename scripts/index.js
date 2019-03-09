7fae5eb0fd69d20ab50544101c6b1780981a9ee7
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-09 17:42:42 -0500

    added a functioning list of clickable names

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,55 @@
 const GoTlist = document.querySelector('[data-list]');
+const GoTdata = document.querySelector('[data-info]');
+const GoTletters = document.querySelector('[data-letters]');
+
 let GoTnameList = [];
+let GoTalphabet = [];
 let index = 0;
+
+
+function OpenLetter(){
+    console.log("HELLO")
+}
+
+function  genAlphabet(charA, charZ){
+    let a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    let myIndex = 0;
+    for (; i <= j; i++){
+        a.push(String.fromCharCode(i));
+        GoTalphabet.push((aDiv = document.createElement('div')));
+        GoTalphabet[myIndex].textContent = String.fromCharCode(i);
+        GoTletters.appendChild(GoTalphabet[myIndex]);
+        GoTalphabet[myIndex].addEventListener('click', OpenLetter);
+        GoTalphabet[myIndex].style.display = 'inline-block';
+        myIndex += 1;
+    };
+    console.log(GoTalphabet)
+    return;
+}
+const myAlphabet = genAlphabet("A","Z");
+
 
 characters.forEach(function (charObj){
     GoTnameList.push((aDiv = document.createElement('div')));
     GoTnameList[index].textContent = charObj.name;
-    GoTnameList[index].setAttribute(`data-${index}`,'character');
+    GoTnameList[index].data = charObj;
     GoTlist.appendChild(GoTnameList[index]);
     GoTnameList[index].addEventListener('click', openIt);
     index += 1;
 });
-console.log(GoTnameList);
 
-function openIt(){
-    console.log("YES")
-}
+
+
+
+
+
+
+function openIt(event){
+
+    let myString = ''
+    Object.keys(event.target.data).forEach(function (key){
+        myString += `${key}: ${event.target.data[key]}\r\n`;
+    });
+    GoTdata.textContent = myString;
+
+};
